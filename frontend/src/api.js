32e73d87c0f3e6075f314031556539d76bb862c4
Satyam Mishra <satyyam.misshra1121@gmail.com>
2026-05-11 02:00:04 +0530

    register working

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,4 @@
-export const API_BASE = "http://localhost:5000/api";
+export const API_BASE = "https://ledgerlink-2.onrender.com/api";
 
 async function request(path, options = {}) {
   const headers = {
